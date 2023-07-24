94c4968d62f9a4d0f7dc30f1525f1e7777d6331a
felixchua3143 <felixchua3142@gmail.com>
2023-07-24 22:13:06 +0800

    s

--- products.js
+++ products.js
@@ -27,4 +27,32 @@ const products = [
         instock: 100,
         imgSrc: "images/peripherals-item-4.png",
     },
+    {
+        id: 4,
+        name: "ElementOne Hydrogen 24HFI 24″ / 1080P / 75Hz / 1ms / IPS",
+        price: 100,
+        instock: 100,
+        imgSrc: "images/monitor-item-1.png",
+    },
+    {
+        id: 5,
+        name: "ElementOne Mercury 27HGCV 27″ / 1080p / 240hZ / 1ms / VA Curved",
+        price: 100,
+        instock: 100,
+        imgSrc: "images/monitor-item-2.png",
+    },
+    {
+        id: 6,
+        name: "ElementOne Mercury 34HGCV 34″ / UW / 1440P / 144Hz / 1ms / VA Curved",
+        price: 100,
+        instock: 100,
+        imgSrc: "images/monitor-item-3.png",
+    },
+    {
+        id: 6,
+        name: "GAM 284FHUE ULTRA",
+        price: 100,
+        instock: 100,
+        imgSrc: "images/monitor-item-4.png",
+    },
 ]
