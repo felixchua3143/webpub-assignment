dd5f6ca364cc2713a79fb88135040c3d18603e1a
felixchua3143 <felixchua3142@gmail.com>
2023-07-24 22:51:09 +0800

    s

--- javascript files/laptop-product.js
+++ javascript files/laptop-product.js
@@ -2,28 +2,28 @@ const products = [
     {
         id: 12,
         name: "LaterShock Fast AE- Sports Battlestation",
-        price: 1899,
+        price: 199,
         instock: 100,
         imgSrc: "images/laptop-item-1.png",
     },
     {
         id: 13,
         name: "NightCore Onion",
-        price: 1650,
+        price: 399,
         instock: 100,
         imgSrc: "images/laptop-item-2.png",
     },
     {
         id: 14,
         name: "NightCore Ghost",
-        price: 2000,
+        price: 649,
         instock: 100,
         imgSrc: "images/laptop-item-3.png",
     },
     {
         id: 15,
         name: "LaterShock Chewing Gum",
-        instock: 2100,
+        instock: 900,
         imgSrc: "images/laptop-item-4.png",
     },
 ]
