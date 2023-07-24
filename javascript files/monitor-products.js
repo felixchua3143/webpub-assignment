34c393f08775f512c06334497ea8f67699421c2e
felixchua3143 <felixchua3142@gmail.com>
2023-07-24 22:46:45 +0800

    s

--- javascript files/monitor-products.js
+++ javascript files/monitor-products.js
@@ -1,27 +1,27 @@
 const products = [
     {
-        id: 0,
+        id: 4,
         name: "ElementOne Hydrogen 24HFI 24″ / 1080P / 75Hz / 1ms / IPS",
         price: 100,
         instock: 100,
         imgSrc: "images/monitor-item-1.png",
     },
     {
-        id: 1,
+        id: 5,
         name: "ElementOne Mercury 27HGCV 27″ / 1080p / 240hZ / 1ms / VA Curved",
         price: 100,
         instock: 100,
         imgSrc: "images/monitor-item-2.png",
     },
     {
-        id: 2,
+        id: 6,
         name: "ElementOne Mercury 34HGCV 34″ / UW / 1440P / 144Hz / 1ms / VA Curved",
         price: 100,
         instock: 100,
         imgSrc: "images/monitor-item-3.png",
     },
     {
-        id: 3,
+        id: 7,
         name: "GAM 284FHUE ULTRA",
         price: 100,
         instock: 100,
--- javascript files/system-products.js
+++ javascript files/system-products.js
@@ -1,27 +1,27 @@
 const products = [
     {
-        id: 0,
+        id: 8,
         name: "LaterShock Fast AE- Sports Battlestation",
         price: 1899,
         instock: 100,
         imgSrc: "images/system-item-1.jpg",
     },
     {
-        id: 1,
+        id: 9,
         name: "NightCore Onion",
         price: 1650,
         instock: 100,
         imgSrc: "images/system-item-2.jpg",
     },
     {
-        id: 2,
+        id: 10,
         name: "NightCore Ghost",
         price: 2000,
         instock: 100,
         imgSrc: "images/system-item-3.jpg",
     },
     {
-        id: 3,
+        id: 11,
         name: "LaterShock Chewing Gum",
         instock: 2100,
         imgSrc: "images/system-item-4.jpg",
