7275c6b57d63698d421f5f9f2082bebe3fc871ff
felixchua3143 <felixchua3142@gmail.com>
2023-07-25 19:39:11 +0800

    s

---
+++ javascript files/index-products.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 8,
+        name: "LaterShock Fast AE- Sports Battlestation",
+        price: 1899,
+        instock: 100,
+        imgSrc: "images/system-item-1.jpg",
+    },
+    {
+        id: 7,
+        name: "GAM 284FHUE ULTRA",
+        price: 100,
+        instock: 100,
+        imgSrc: "images/monitor-item-4.png",
+    },
+    {
+        id: 1,
+        name: "Shaver Naga v2 Pro",
+        price: 100,
+        instock: 100,
+        imgSrc: "images/peripherals-item-2.png",
+    },
+]
